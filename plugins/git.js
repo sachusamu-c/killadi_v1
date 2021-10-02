bca8222af67b304a9f2a65113f81754109dd79bd
HI <87406580+sachusamu-c@users.noreply.github.com>
2021-10-02 09:45:53 +0530

    🍃𝐊𝐈𝐋𝐋𝐀𝐃𝐈 𝐁𝐎𝐓🦋

--- plugins/git.js
+++ plugins/git.js
@@ -11,18 +11,18 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/dwQR2AL.jpeg";
+  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
-*Creator number : wa.me/919744474996?text=Hi%20sachu%20samu.%20*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is sachusamu*
+*Creator number : wa.me/919744474997?text=Hi%20Sachu%20Samu.%20*
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
 *Githublink (Setup)  :    https://github.com/sachusamu-c/killadi_v1*
-*Audio commads :   https://github.com/sachusamu-c/killadi_v1/tree/master/uploads*
+*Audio commads :   https://github.com/sachusamu-c/tree/master/uploads*
 *Sticker commads : https://github.com/sachusamu-c/killadi_v1/tree/master/uploads*
 `}) 
 
